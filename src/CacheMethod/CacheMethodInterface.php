4dabb6f97bb1907773741090ef228c831ade5af7
Rob Frawley 2nd <rfrawley@scribenet.com>
2015-03-01 00:22:30 -0500

    updated integration files

---
+++ src/CacheMethod/CacheMethodInterface.php
@@ -0,0 +1,23 @@
+<?php
+/*
+ * This file is part of the Scribe Cache Bundle.
+ *
+ * (c) Scribe Inc. <source@scribe.software>
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
+namespace Scribe\CacheBundle\CacheMethod;
+
+/**
+ * Interface CacheMethodInterface
+ *
+ * @package Scribe\CacheBundle\CacheMethod
+ */
+interface CacheMethodInterface
+{
+
+}
+
+/* EOF */
